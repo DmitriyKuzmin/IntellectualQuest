8cff3a6456a67a37fa6a46d09ebd0b523aef64b6
DmitriyKuzmin <dmitriy.kuzmin910@gmail.com>
2019-03-22 22:16:32 +0300

    Refactoring MainActivity

--- app/src/main/java/com/kdp/quest/MainActivity.java
+++ app/src/main/java/com/kdp/quest/MainActivity.java
@@ -1,9 +1,5 @@
 package com.kdp.quest;
 
-
-import android.app.ActivityManager;
-import android.content.Context;
-import android.content.pm.ConfigurationInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,7 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import com.kdp.quest.fragment.CameraFragment;
 import com.kdp.quest.fragment.InfoFragment;
@@ -34,63 +29,44 @@ public class MainActivity extends ARActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
-        toolbar = getSupportActionBar();
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        toolbar.setTitle("Camera");
+        toolbar = getSupportActionBar();
+        toolbar.setTitle(getString(R.string.title_camera));
         loadFragment(CameraFragment.getInstance());
 
-        ArrayList<Target> targets = new ArrayList<>();
-
-        targets.add(new Target("Robot", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices odio sed neque interdum pellentesque. Fusce sed sapien neque. Pellentesque consectetur, elit nec commodo molestie, nisi odio convallis lectus, quis sodales libero nibh non urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque accumsan eleifend enim, eget luctus leo tincidunt ac. Donec non metus turpis. Cras at mauris velit. Nam semper dapibus est, at interdum dui maximus ut."));
-        targets.add(new Target("ClearCode", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices odio sed neque interdum pellentesque. Fusce sed sapien neque. Pellentesque consectetur, elit nec commodo molestie, nisi odio convallis lectus, quis sodales libero nibh non urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque accumsan eleifend enim, eget luctus leo tincidunt ac. Donec non metus turpis. Cras at mauris velit. Nam semper dapibus est, at interdum dui maximus ut."));
-        targets.add(new Target("Kish", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultrices odio sed neque interdum pellentesque. Fusce sed sapien neque. Pellentesque consectetur, elit nec commodo molestie, nisi odio convallis lectus, quis sodales libero nibh non urna. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Quisque accumsan eleifend enim, eget luctus leo tincidunt ac. Donec non metus turpis. Cras at mauris velit. Nam semper dapibus est, at interdum dui maximus ut."));
-
-        TargetManager.getInstance(targets);
-
-
-        ArrayList<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("1", "95"));
-        tasks.add(new Task("2", "4"));
-        tasks.add(new Task("3", "120"));
-        TaskManager.getInstance(tasks);
-
-
-        List<String> trackingFileName = TargetManager.getInstance(null).getTrackingFileName();
-        for (String s : trackingFileName) {
-            TrackerManager.getInstance().addTrackerData(s, true);
-        }
-        TrackerManager.getInstance().loadTrackerData();
+        initializeTargets();
+        initializeTasks();
+        loadTrackerData();
     }
 
     private OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = new OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            Fragment fragment;
+            Fragment fragment = null;
             switch (menuItem.getItemId()) {
                 case R.id.navigation_camera:
                     toolbar.setTitle(getString(R.string.title_camera));
                     fragment = CameraFragment.getInstance();
-                    loadFragment(fragment);
-                    return true;
+                    break;
                 case R.id.navigation_info:
                     toolbar.setTitle(getString(R.string.title_info));
                     fragment = new InfoFragment();
-                    loadFragment(fragment);
-                    return true;
-
+                    break;
                 case R.id.navigation_target:
                     toolbar.setTitle(getString(R.string.title_target));
                     fragment = new TargetFragment();
-                    loadFragment(fragment);
-                    return true;
+                    break;
             }
-            return false;
+
+            if (fragment == null)
+                return false;
+
+            loadFragment(fragment);
+            return true;
         }
     };
 
@@ -101,10 +77,30 @@ public class MainActivity extends ARActivity {
      */
     private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.frame_container, fragment, "CameraFragment");
+        transaction.replace(R.id.frame_container, fragment);
         transaction.addToBackStack(null);
         transaction.commit();
     }
 
+    private void initializeTargets() {
+        ArrayList<Target> targets = new ArrayList<>();
+        targets.add(new Target("Robot", "Lorem ipsum dolor."));
+        targets.add(new Target("ClearCode", "Lorem ipsum dolor."));
+        TargetManager.getInstance(targets);
+    }
 
+    private void initializeTasks() {
+        ArrayList<Task> tasks = new ArrayList<>();
+        tasks.add(new Task("1", "95"));
+        tasks.add(new Task("2", "4"));
+        TaskManager.getInstance(tasks);
+    }
+
+    private void loadTrackerData() {
+        List<String> trackingFileName = TargetManager.getInstance(null).getTrackingFileName();
+        for (String s : trackingFileName) {
+            TrackerManager.getInstance().addTrackerData(s, true);
+        }
+        TrackerManager.getInstance().loadTrackerData();
+    }
 }
